5015f0e6b039d9f61f8c8fab272efb408b15623c
moassaad <mohammadasaadgo@gmail.com>
2024-12-15 09:54:33 +0200

    create: routes api.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\Auth\LoginController;
+use App\Http\Controllers\API\Auth\LogoutController;
+use App\Http\Controllers\API\Auth\RegisterController;
+use App\Http\Controllers\API\CatigoryController;
+use App\Http\Controllers\API\TaskController;
+use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\GeneralController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +20,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware(['guest:sanctum'])
+    ->group(function(){
+        Route::get('/test',[GeneralController::class,'test']);
+        Route::controller(LoginController::class)
+            ->group(function(){
+                Route::post('/login', 'login')->name('login');
+            }
+        );
+        Route::controller(RegisterController::class)
+            ->group(function(){
+                Route::post('/register', 'register');
+            }
+        );
+    }
+);
+
+Route::middleware(['auth:sanctum'])
+    ->group(function(){
+        Route::get('/logout', [LogoutController::class,'logout']);
+        Route::get('/profile', [UserController::class,'profile']);
+        
+        Route::controller(TaskController::class)
+        ->group(function () {
+                Route::patch('/task/completion/{task}','completion');
+                Route::get('/task/today','taskToday');
+                Route::get('/task/filter/catigory/{catigory_id}','filterCatigory');
+                Route::get('/task/filter/completion/{status}','filterCompletion')->name('task.filter.completion');
+            }
+        );
+        Route::resource('task',TaskController::class);
+
+        Route::resource('catigory',CatigoryController::class);
+    }
+);
