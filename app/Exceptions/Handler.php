08b7051734841ec8adc01a36c452b7b390a4c4d2
moassaad <mohammadasaadgo@gmail.com>
2024-12-15 09:54:09 +0200

    create: register exceptions.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,9 +2,18 @@
 
 namespace App\Exceptions;
 
+use App\Services\APIResponse;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
+
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 class Handler extends ExceptionHandler
 {
     /**
@@ -41,8 +50,64 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        // $this->reportable(function (Throwable $e) {
+        //     //
+        // });
+        $this->renderable(function (AuthorizationException $exception, Request $request) 
+        {
+            if($request->is('api/*'))
+            {
+                return APIResponse::new()->errorForbidden($exception->getMessage(), [$exception->asNotFound()]);
+            }
+        });
+       $this->renderable(function (AuthenticationException $exception, Request $request) 
+       {
+           if($request->is('api/*'))
+           {
+               return APIResponse::new()->errorUnauthorized($exception->getMessage(), ["Access Denied! You don't have permission."]);
+           }
+       });
+        $this->renderable(function (HttpResponseException $exception, Request $request) 
+        {
+            if($request->is('api/*'))
+            {
+                return APIResponse::new()->error(501,$exception->getMessage(), [$exception->getFile()]);
+            }
+        });
+        $this->renderable(function (ValidationException $exception, Request $request) 
+        {
+            if($request->is('api/*'))
+            {
+                return APIResponse::new()->error($exception->status,$exception->getMessage(), [$exception->errors(),$exception->errorBag($exception->getMessage())]);
+            }
+        });
+        $this->renderable(function (NotFoundHttpException $exception, Request $request) 
+        {
+            if($request->is('api/*'))
+            {
+                return APIResponse::new()->errorNotFound($exception->getMessage());
+            }
+        });
+        $this->renderable(function (MethodNotAllowedException $exception, Request $request) 
+        {
+            if($request->is('api/*'))
+            {
+                return APIResponse::new()->error(405,$exception->getMessage(), ["error"]);
+            }
+        });
+        $this->renderable(function (\RuntimeException $exception, Request $request) 
+        {
+            if($request->is('api/*'))
+            {
+                return APIResponse::new()->errorInternalServerError($exception->getMessage(), [$exception->getPrevious()]);
+            }
+        });
+        $this->renderable(function (\Exception $exception, Request $request) 
+        {
+            if($request->is('api/*'))
+            {
+                return APIResponse::new()->errorInternalServerError($exception->getMessage(), [$exception->getTrace()]);
+            }
         });
     }
 }
